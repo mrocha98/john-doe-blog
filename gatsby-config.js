e51e225a2b41803f94b9b2cc0daf5cd5879c436d
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-23 15:24:48 -0300

    cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `John Doe blog`,
+    description: `A blog when John Doe writes cool stuff`,
+    author: `John Doe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
