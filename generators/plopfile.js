060741273bbe139698db9d7f76df2df2c279c276
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-23 19:30:42 -0300

    setup plop to generate components

---
+++ generators/plopfile.js
@@ -0,0 +1,31 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{kebabCase name}}.component.tsx',
+        templateFile: 'templates/component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/index.ts.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{kebabCase name}}.styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      }
+    ]
+  })
+}
