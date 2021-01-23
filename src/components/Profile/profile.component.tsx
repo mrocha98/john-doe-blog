bc67281e559cfd4777cf297f2dca7314c219c48a
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-23 17:06:09 -0300

    enable prettier config in vscode

--- src/components/Profile/profile.component.tsx
+++ src/components/Profile/profile.component.tsx
@@ -9,10 +9,12 @@ export const Profile = ({}: ProfileProps) => {
     <S.Wraper>
       <h1>John Doe</h1>
       <h2>Professional Imaginary Person</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit in 
-        recusandae ipsam quis amet doloribus, culpa quaerat dolores, provident 
-        ipsa atque, illo unde repudiandae. Nostrum nisi nihil perspiciatis 
-        beatae corrupti?</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit in
+        recusandae ipsam quis amet doloribus, culpa quaerat dolores, provident
+        ipsa atque, illo unde repudiandae. Nostrum nisi nihil perspiciatis
+        beatae corrupti?
+      </p>
     </S.Wraper>
   )
 }
